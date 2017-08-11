31e1df502434738634cb84c7553a0659a3313032
Unknown <vincent.starck@gmail.com>
2017-08-10 22:29:27 -0500

    pruebas con modal

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import {ModalController} from 'ionic-angular';
+import {ListPage} from '../list/list';
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
-  constructor() {
+  constructor(public modalCtrl: ModalController) {
 
   }
 
   doLogin(){
     console.log("do login");
+    let modal=this.modalCtrl.create(ListPage);
+    modal.present();
   }
   doSignup(){
      console.log("do doSignup");
